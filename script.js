62b90bd834ebb4203f535e1b4f6e5057045e4d5a
DivyanshuSahu <dsahu1997@gmail.com>
2017-12-27 21:11:27 +0530

    updated

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
 var canvas;
 var canvasContext;
-const FRAMES = 25;
+const FRAMES = 20;
 const BALL_RADIUS = 8;
 var ballX = BALL_RADIUS + 10; // set ball at bottom initially
 var ballY = 150 - BALL_RADIUS; // canvas.height - BALL_RADIUS
-const CONSTANT_GAP_VERTICAL = 80;
+const CONSTANT_GAP_VERTICAL = 60;
 const CONSTANT_GAP_HORIZONTAL = 200;
 const OBSTACLES_WIDTH = 20;
-var BALLSPEEDUP = 10;
-var BALLDOWNSPEED = 1;
-const OBSTACLES_SPEED = 1;
+var BALLSPEEDUP = 20;
+var BALLDOWNSPEED = 0;
+const OBSTACLES_SPEED = 5;
 var pos = 200;
 var a = [];
+var score = 0;
 
 window.onload = function() {
     canvas = document.getElementById("game");
     canvasContext = canvas.getContext('2d');
     setInterval(callEverything, 1000 / FRAMES);
-    //callEverything();
 }
 
 function callEverything() {
     draw();
 }
 
-//var top = Math.floor(Math.random() * 240) + 1;
 function obstaclesHeight() {
-	for (let i=0;i<5;i++) {
+	let a = [];
+	for (let i=0;i<1000;i++) {
 		a.push(Math.floor(Math.random()*240) + 1)
 	}
-	console.log(a);
+	return a;
 }
 
-obstaclesHeight();
+a =  obstaclesHeight();
+
+function checkPass(x) {
+	if (ballY > x && ballY < (x + CONSTANT_GAP_VERTICAL)) {
+		return true;
+	}
+	conole.log("no");
+	return false;
+}
+
+function currentObstacle(i) {
+	if (i < 200)
+		return true;
+	return false;
+}
 
 function drawObstacles(pos) {
     canvasContext.fillStyle = '#4caf50';
     let i = pos;
+    let count = 0;
     while( i < canvas.width) {
-    	canvasContext.fillRect(i, 0, OBSTACLES_WIDTH, a[0]);
-    	canvasContext.fillRect(i, a[0] + CONSTANT_GAP_VERTICAL, OBSTACLES_WIDTH, canvas.height - (a[0] + CONSTANT_GAP_VERTICAL));
+    	count++;
+    	canvasContext.fillRect(i, 0, OBSTACLES_WIDTH, a[count]);
+    	canvasContext.fillRect(i, a[count] + CONSTANT_GAP_VERTICAL, OBSTACLES_WIDTH, canvas.height - (a[count] + CONSTANT_GAP_VERTICAL));
     	i += CONSTANT_GAP_HORIZONTAL;
-    	//canvasContext.fillRect(i, 0, OBSTACLES_WIDTH, 100);
-    	//canvasContext.fillRect(i, 100 + CONSTANT_GAP_VERTICAL, OBSTACLES_WIDTH, canvas.height - 150);
-    	//canvasContext.fillRect(i + 2*CONSTANT_GAP_HORIZONTAL, 0, OBSTACLES_WIDTH, 100);
-    	//canvasContext.fillRect(i + 2*CONSTANT_GAP_HORIZONTAL, 100 + CONSTANT_GAP_VERTICAL, OBSTACLES_WIDTH, canvas.height - 150);
-    	//canvasContext.fillRect(i + 3*CONSTANT_GAP_HORIZONTAL, 0, OBSTACLES_WIDTH, 100);
-    	//canvasContext.fillRect(i + 3*CONSTANT_GAP_HORIZONTAL, 100 + CONSTANT_GAP_VERTICAL, OBSTACLES_WIDTH, canvas.height - 150);
-    	//canvasContext.fillRect(i + 4*CONSTANT_GAP_HORIZONTAL, 0, OBSTACLES_WIDTH, 100);
-    	//canvasContext.fillRect(i + 4*CONSTANT_GAP_HORIZONTAL, 100 + CONSTANT_GAP_VERTICAL, OBSTACLES_WIDTH, canvas.height - 150);
+    	if (count == 1000) {
+    		count = 0;
+    	}
     }
 }
 
